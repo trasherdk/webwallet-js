b6788b16b4ba373d786f9b1c31b7b42749871842
Admin <qwertycoin@protonmail.com>
2019-01-19 20:15:15 +0100

    Initial Qwertycoin Settings

--- src/config.ts
+++ src/config.ts
@@ -1,35 +1,35 @@
 let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
 	apiUrl:typeof window !== 'undefined' && window.location ? window.location.href.substr(0,window.location.href.lastIndexOf('/')+1)+'api/' : 'https://wallet.karbo.org/api/',
-	mainnetExplorerUrl: "http://explorer.karbowanec.com/",
-	mainnetExplorerUrlHash: "http://explorer.karbowanec.com/?hash={ID}#blockchain_transaction",
-	mainnetExplorerUrlBlock: "http://explorer.karbowanec.com/?hash={ID}#blockchain_block",
-	testnetExplorerUrl: "http://testnet.karbo.org/",
-	testnetExplorerUrlHash: "http://testnet.karbo.org/?hash={ID}#blockchain_transaction",
-	testnetExplorerUrlBlock: "http://testnet.karbo.org/?hash={ID}#blockchain_block",
+	mainnetExplorerUrl: "https://explorer.qwertycoin.org/",
+	mainnetExplorerUrlHash: "https://explorer.qwertycoin.org/?hash={ID}#blockchain_transaction",
+	mainnetExplorerUrlBlock: "https://explorer.qwertycoin.org/?hash={ID}#blockchain_block",
+	testnetExplorerUrl: "https://testnet.qwertycoin.org/",
+	testnetExplorerUrlHash: "https://testnet.qwertycoin.org/?hash={ID}#blockchain_transaction",
+	testnetExplorerUrlBlock: "https://testnet.qwertycoin.org/?hash={ID}#blockchain_block",
 	testnet: false,
-    coinUnitPlaces: 12,
+    coinUnitPlaces: 8,
     coinDisplayUnitPlaces: 2,
-	txMinConfirms: 10,         
-	txCoinbaseMinConfirms: 10,
-	addressPrefix: 111,
+	txMinConfirms: 6,         
+	txCoinbaseMinConfirms: 6,
+	addressPrefix: 1344012,
 	integratedAddressPrefix: 112,
-	addressPrefixTestnet: 111,
+	addressPrefixTestnet: 1344012,
 	integratedAddressPrefixTestnet: 112,
 	subAddressPrefix: 113,
 	subAddressPrefixTestnet: 113,
-	coinFee: new JSBigInt('100000000000'),
-	feePerKB: new JSBigInt('100000000000'), //for testnet its not used, as fee is dynamic.
-	dustThreshold: new JSBigInt('100000000'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	coinFee: new JSBigInt('100000000'),
+	feePerKB: new JSBigInt('100000000'), //for testnet its not used, as fee is dynamic.
+	dustThreshold: new JSBigInt('100000'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
 	defaultMixin: 0, // default value mixin
 
 	idleTimeout: 30,
 	idleWarningDuration: 20,
 
-	coinSymbol: 'KRB',
-	openAliasPrefix: "krb",
-	coinName: 'Karbo',
-	coinUriPrefix: 'karbowanec:',
-	avgBlockTime: 240,
+	coinSymbol: 'QWC',
+	openAliasPrefix: "qwc",
+	coinName: 'Qwertycoin',
+	coinUriPrefix: 'qwertycoin:',
+	avgBlockTime: 120,
 	maxBlockNumber: 500000000,
 };
--- src/model/WalletRepository.ts
+++ src/model/WalletRepository.ts
@@ -184,11 +184,11 @@ export class WalletRepository{
 		doc.setTextColor(255, 255, 255);
 		doc.setFontSize(10);
 		doc.text(110, 120, "To deposit funds to this paper wallet, send ");
-		doc.text(110, 125, "Karbo to the public address");
+		doc.text(110, 125, "Qwertycoin to the public address");
 
 		doc.text(110, 135, "DO NOT REVEAL THE PRIVATE KEY");
 
-		//adding karbo logo
+		//adding qwertycoin logo
 		let c : HTMLCanvasElement|null = <HTMLCanvasElement>document.getElementById('canvasExport');
 		if(c !== null) {
 			let ctx = c.getContext("2d");
