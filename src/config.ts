159e180774eddd549b9ebe1011e95f011a2d665a
nnian <alexnnian@protonmail.com>
2019-06-27 11:59:26 +0200

    Add Contacts and Elections...
    ...Implementation of the Contact Function to add and edit Contacts inside your Web Wallet for easier transfer your Coins to your Friends.
    
    -Add the voting.qwertycoin.org API to vote inside your web wallet to lead this Project.
    
    Please use your rights to lead the community.

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,12 @@
-let global : any = typeof window !== 'undefined' ? window : self;
+let global: any = typeof window !== 'undefined' ? window : self;
 global.config = {
 	apiUrl: [
 		"https://03.cache.myqwertycoin.com/",
 		"https://02.cache.myqwertycoin.com/",
 		"https://01.cache.myqwertycoin.com/"
 	],
+	electionApiUrl: "https://voting.qwertycoin.org/api",
+	websiteApiUrl: "https://www.qwertycoin.org/wp-json",
 	mainnetExplorerUrl: "https://explorer.qwertycoin.org/",
 	mainnetExplorerUrlHash: "https://explorer.qwertycoin.org/?hash={ID}#blockchain_transaction",
 	mainnetExplorerUrlBlock: "https://explorer.qwertycoin.org/?hash={ID}#blockchain_block",
@@ -12,9 +14,9 @@ global.config = {
 	testnetExplorerUrlHash: "https://testnet.qwertycoin.org/?hash={ID}#blockchain_transaction",
 	testnetExplorerUrlBlock: "https://testnet.qwertycoin.org/?hash={ID}#blockchain_block",
 	testnet: false,
-    coinUnitPlaces: 8,
-    coinDisplayUnitPlaces: 8,
-	txMinConfirms: 10,         
+	coinUnitPlaces: 8,
+	coinDisplayUnitPlaces: 8,
+	txMinConfirms: 10,
 	txCoinbaseMinConfirms: 10,
 	addressPrefix: 0x14820c,
 	integratedAddressPrefix: 0x148201,
@@ -24,7 +26,7 @@ global.config = {
 	subAddressPrefixTestnet: 0x148202,
 	coinFee: new JSBigInt('100000000'),
 	feePerKB: new JSBigInt('100000000'),
-	dustThreshold: new JSBigInt('100000'),//used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
+	dustThreshold: new JSBigInt('100000'), //used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
 	defaultMixin: 0, // default value mixin
 
 	idleTimeout: 30,
