411135d2670c7031b3fa60d6f1fe45776a1451bc
tommR1 <76653019+tommR1@users.noreply.github.com>
2021-01-08 00:24:33 +0100

    CI changes

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ global.config = {
 	nodeList: [
 		"https://api.societatis.io/sslnode/"
 	],
-	electionApiUrl: "https://voting.societatis.io/api",
+	electionApiUrl: "https://voting.qwertycoin.org/api",
 	websiteApiUrl: "https://societatis.io/wp-json",
 	mainnetExplorerUrl: "https://explorer.societatis.io/",
 	mainnetExplorerUrlHash: "https://explorer.societatis.io/?hash={ID}#blockchain_transaction",
@@ -17,11 +17,11 @@ global.config = {
 	txMinConfirms: 10,
 	txCoinbaseMinConfirms: 10,
 	addressPrefix: 0x199296,
-	integratedAddressPrefix: 0x148201,
+	integratedAddressPrefix: 0x199296,
 	addressPrefixTestnet: 0x199296,
-	integratedAddressPrefixTestnet: 0x148201,
-	subAddressPrefix: 0x148202,
-	subAddressPrefixTestnet: 0x148202,
+	integratedAddressPrefixTestnet: 0x199297,
+	subAddressPrefix: 0x199295,
+	subAddressPrefixTestnet: 0x199295,
 	coinFee: new JSBigInt('100000000'),
 	feePerKB: new JSBigInt('100000000'),
 	dustThreshold: new JSBigInt('100000'), //used for choosing outputs/change - we decompose all the way down if the receiver wants now regardless of threshold
